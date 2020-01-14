7ed17a8c2322709186c03e23451cb00421326da0
Your Name <you@example.com>
2020-01-13 22:22:10 -0800

    Start of personal project

---
+++ u_personal_project/Assets/Scripts/MoveUpAndDown.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveUpAndDown : MonoBehaviour
+{
+    private float max_height = 3.0f;
+    private int move_dir = 1;
+    public float speed = 3.0f;
+    private Vector3 start_pos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        start_pos = transform.position; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(start_pos.y - transform.position.y > max_height || start_pos.y - transform.position.y < -max_height)
+        {
+            move_dir *= -1;
+        }
+
+        transform.Translate(Vector3.up * move_dir * speed * Time.deltaTime, Space.World);
+    }
+}
---
+++ u_personal_project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float h_input;
+    public float move_speed = 5.0f;
+    private Vector3 speed = new Vector3(0,0,0);
+    public Rigidbody rb;
+    //private float v_input;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speed = rb.velocity;
+        h_input = Input.GetAxis("Horizontal");
+        transform.Translate( new Vector3(1,0,0) * h_input * move_speed * Time.deltaTime, Space.World);
+    }
+}
